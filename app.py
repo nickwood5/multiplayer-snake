fa1d8d3cb48aed658699d54dbaa25985f7b4c526
Nicholas Wood <horizon.nicholas14@gmail.com>
2022-04-23 13:40:00 -0400

    Update app script

--- app.py
+++ app.py
@@ -2,6 +2,21 @@ print("Run app")
 import time
 
 import websockets
+
+import asyncio
+print("s")
+
+async def echo(websocket):
+    async for message in websocket:
+        print(message)
+        await websocket.send(message)
+
+async def main():
+    async with websockets.serve(echo, "multiplayer-snake-git-nickwood5-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com", 8080):
+        await asyncio.Future()  # run forever
+
+asyncio.run(main())
+
 while (1):
     time.sleep(10)
     print("Run ap 1p")
