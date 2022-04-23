35605b10655a9f3e39c3e8c8682c703233f77179
Nicholas Wood <horizon.nicholas14@gmail.com>
2022-04-23 02:54:10 -0400

    Edit app.py to add debug prints

--- app.py
+++ app.py
@@ -1,15 +1,2 @@
 print("Run app")
 
-import asyncio
-import websockets
-
-async def echo(websocket):
-    async for message in websocket:
-        print(message)
-        await websocket.send(message)
-
-async def main():
-    async with websockets.serve(echo, "https://multiplayer-snake-git-nickwood5-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/", 8080):
-        await asyncio.Future()  # run forever
-
-asyncio.run(main())
