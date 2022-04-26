57268b29bb899fb95493d50ed9ea0344412464c6
Nicholas Wood <horizon.nicholas14@gmail.com>
2022-04-25 23:39:00 -0400

    Update pod ip address

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ async def head():
 async def main2():
     print("Main2")
     print(time.time())
-    async with websockets.serve(echo2, "127.0.0.1", 8765):
+    async with websockets.serve(echo2, "10.131.2.84", 8765):
         await asyncio.Future()  # run forever
 
 async def local_websocket():
