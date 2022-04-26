eaa8fe804a8f1bb6c43b971fa25d342b578ecbda
Nicholas Wood <horizon.nicholas14@gmail.com>
2022-04-25 23:13:49 -0400

    Update local api route

--- apitest.py
+++ apitest.py
@@ -1,5 +1,5 @@
 import requests
 
-a = requests.get("http://test2-nickwood5-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/ping")
+a = requests.get("http://multiplayer-snake-api-nickwood5-dev.apps.sandbox-m2.ll9k.p1.openshiftapps.com/")
 
-print(a)
+print(a.json())
